e03e1a81618fd55a245d63cac4ef4ccb37326f63
KwangSoo Yoo <kwangsoo.yoo@gmail.com>
2020-04-21 23:17:35 +0900

    SUM 함수 추가

---
+++ MVCDemo/Controllers/CalcController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers {
+    public class CalcController : Controller {
+        // GET: Calc
+        public ActionResult Index() {
+            return View();
+        }
+
+
+        public ActionResult Sum() {
+            return View();
+        }
+    }
+}
